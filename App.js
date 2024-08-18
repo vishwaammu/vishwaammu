dec3f8c6a83b7d25bf7052a993a5376c8040fc37
santhoshima <santhoshima.mani@gmail.com>
2024-08-18 15:26:20 +0530

    dgkljgkldjkl

---
+++ App.js
@@ -0,0 +1,27 @@
+/**
+ * 
+ *<div id="parent">
+*  <div id="child">
+*     <h1>I'm h1 tag</h1>
+*    <h2>I'm h2 tag</h2>
+*     </div>
+*</div>
+ * 
+ * ReactElement(Object) =>HTML(Browser Understands)
+ */
+
+const parent = React.createElement("div", {id: "parent"},[
+    React.createElement("div", {id: "child"},[
+        React.createElement{"h1", {}, "I'm an h1 tag"},
+        React.createElement{"h2", {}, "I'm an h2 tag"},
+    ]),
+]);
+
+
+//JSX
+
+console.log(parent); //object
+
+const root = ReactDOM.createRoot(document.getElementById("header"));
+
+root.render(parent);
